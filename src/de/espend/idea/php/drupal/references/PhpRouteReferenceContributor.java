de6388bc43d716f46e0e347622a8502a2daff14e
Haehnchen <daniel@espendiller.net>
2014-11-22 10:15:07 +0100

    add new route method signatures also support Drupal\Core\Url #2

--- src/de/espend/idea/php/drupal/references/PhpRouteReferenceContributor.java
+++ src/de/espend/idea/php/drupal/references/PhpRouteReferenceContributor.java
@@ -4,7 +4,7 @@ import com.intellij.patterns.PlatformPatterns;
 import com.intellij.psi.*;
 import com.intellij.util.ProcessingContext;
 import com.jetbrains.php.lang.PhpLanguage;
-import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import com.jetbrains.php.lang.psi.elements.*;
 import de.espend.idea.php.drupal.DrupalProjectComponent;
 import fr.adrienbrault.idea.symfony2plugin.routing.RouteReference;
 import fr.adrienbrault.idea.symfony2plugin.util.MethodMatcher;
@@ -13,10 +13,12 @@ import org.jetbrains.annotations.NotNull;
 public class PhpRouteReferenceContributor extends PsiReferenceContributor {
 
     public static MethodMatcher.CallToSignature[] GENERATOR_SIGNATURES = new MethodMatcher.CallToSignature[] {
-        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Routing\\UrlGeneratorInterface", "getPathFromRoute"),
-        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Routing\\UrlGeneratorInterface", "generateFromRoute"),
+        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Routing\\UrlGeneratorInterface", "getPathFromRoute"), // <- <@TODO: remove: pre Drupal8 beta
+        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Routing\\UrlGeneratorInterface", "generateFromRoute"), // <- <@TODO: remove: pre Drupal8 beta
+        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Routing\\UrlGenerator", "getPathFromRoute"),
+        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Routing\\UrlGenerator", "generateFromRoute"),
+        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Url", "fromRoute"),
     };
-
 
     @Override
     public void registerReferenceProviders(PsiReferenceRegistrar psiReferenceRegistrar) {
@@ -37,6 +39,38 @@ public class PhpRouteReferenceContributor extends PsiReferenceContributor {
                     }
 
                     return new PsiReference[]{ new RouteReference((StringLiteralExpression) psiElement) };
+                }
+            }
+        );
+
+        psiReferenceRegistrar.registerReferenceProvider(
+            PlatformPatterns.psiElement(StringLiteralExpression.class).withLanguage(PhpLanguage.INSTANCE),
+            new PsiReferenceProvider() {
+                @NotNull
+                @Override
+                public PsiReference[] getReferencesByElement(@NotNull PsiElement psiElement, @NotNull ProcessingContext processingContext) {
+
+                    if(!DrupalProjectComponent.isEnabled(psiElement)) {
+                        return new PsiReference[0];
+                    }
+
+                    if(psiElement instanceof StringLiteralExpression) {
+                        PsiElement parameterList = psiElement.getParent();
+                        if(parameterList instanceof ParameterList) {
+                            PsiElement newExpression = parameterList.getParent();
+                            if(newExpression instanceof NewExpression) {
+                                ClassReference classReference = ((NewExpression) newExpression).getClassReference();
+                                if(classReference != null && "Url".equals(classReference.getName())) {
+                                    String fqn = classReference.getFQN();
+                                    if(fqn != null && fqn.equals("\\Drupal\\Core\\Url")) {
+                                        return new PsiReference[]{ new RouteReference((StringLiteralExpression) psiElement) };
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    return new PsiReference[0];
                 }
             }
         );
