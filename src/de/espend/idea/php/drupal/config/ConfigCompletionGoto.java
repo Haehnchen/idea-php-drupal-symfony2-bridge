3df2571260304e7394234e463a3c4945601fa8ab
Haehnchen <daniel@espendiller.net>
2014-11-22 11:06:17 +0100

    add config schema index and completion / goto

---
+++ src/de/espend/idea/php/drupal/config/ConfigCompletionGoto.java
@@ -0,0 +1,133 @@
+package de.espend.idea.php.drupal.config;
+
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.codeInsight.lookup.LookupElementBuilder;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.patterns.PlatformPatterns;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiManager;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.Processor;
+import com.intellij.util.indexing.FileBasedIndex;
+import com.intellij.util.indexing.FileBasedIndexImpl;
+import com.jetbrains.php.lang.PhpLanguage;
+import com.jetbrains.php.lang.psi.elements.StringLiteralExpression;
+import de.espend.idea.php.drupal.DrupalProjectComponent;
+import de.espend.idea.php.drupal.index.CollectProjectUniqueKeys;
+import de.espend.idea.php.drupal.index.ConfigSchemaIndex;
+import fr.adrienbrault.idea.symfony2plugin.Symfony2Icons;
+import fr.adrienbrault.idea.symfony2plugin.codeInsight.GotoCompletionContributor;
+import fr.adrienbrault.idea.symfony2plugin.codeInsight.GotoCompletionProvider;
+import fr.adrienbrault.idea.symfony2plugin.codeInsight.GotoCompletionRegistrar;
+import fr.adrienbrault.idea.symfony2plugin.codeInsight.GotoCompletionRegistrarParameter;
+import fr.adrienbrault.idea.symfony2plugin.util.MethodMatcher;
+import fr.adrienbrault.idea.symfony2plugin.util.PsiElementUtils;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.yaml.YAMLFileType;
+import org.jetbrains.yaml.psi.YAMLDocument;
+import org.jetbrains.yaml.psi.YAMLKeyValue;
+
+import java.util.*;
+
+public class ConfigCompletionGoto implements GotoCompletionRegistrar {
+
+    private static MethodMatcher.CallToSignature[] CONFIG = new MethodMatcher.CallToSignature[] {
+        new MethodMatcher.CallToSignature("\\Drupal\\Core\\Config\\ConfigFactory", "get"),
+    };
+
+    @Override
+    public void register(GotoCompletionRegistrarParameter registrar) {
+        registrar.register(PlatformPatterns.psiElement().withParent(StringLiteralExpression.class).withLanguage(PhpLanguage.INSTANCE), new GotoCompletionContributor() {
+            @Nullable
+            public GotoCompletionProvider getProvider(@NotNull PsiElement psiElement) {
+
+                if(!DrupalProjectComponent.isEnabled(psiElement)) {
+                    return null;
+                }
+
+                PsiElement parent = psiElement.getParent();
+                if(parent == null) {
+                    return null;
+                }
+
+                MethodMatcher.MethodMatchParameter methodMatchParameter = MethodMatcher.getMatchedSignatureWithDepth(parent, CONFIG);
+                if(methodMatchParameter == null) {
+                    return null;
+                }
+
+                return new FormReferenceCompletionProvider(parent);
+
+            }
+        });
+    }
+
+    private static class FormReferenceCompletionProvider extends GotoCompletionProvider {
+
+        public FormReferenceCompletionProvider(PsiElement element) {
+            super(element);
+        }
+
+        @NotNull
+        @Override
+        public Collection<LookupElement> getLookupElements() {
+
+            CollectProjectUniqueKeys ymlProjectProcessor = new CollectProjectUniqueKeys(getProject(), ConfigSchemaIndex.KEY);
+            FileBasedIndex.getInstance().processAllKeys(ConfigSchemaIndex.KEY, ymlProjectProcessor, getProject());
+
+            Collection<LookupElement> lookupElements = new ArrayList<LookupElement>();
+            for(String config: ymlProjectProcessor.getResult()) {
+                lookupElements.add(LookupElementBuilder.create(config).withIcon(Symfony2Icons.CONFIG_VALUE));
+            }
+
+            return lookupElements;
+        }
+
+        @NotNull
+        @Override
+        public Collection<PsiElement> getPsiTargets(PsiElement psiElement) {
+
+            PsiElement element = psiElement.getParent();
+            if(!(element instanceof StringLiteralExpression)) {
+                return Collections.emptyList();
+            }
+
+            final String contents = ((StringLiteralExpression) element).getContents();
+            if(StringUtils.isBlank(contents)) {
+                return Collections.emptyList();
+            }
+
+            final Collection<PsiElement> psiElements = new ArrayList<PsiElement>();
+            FileBasedIndexImpl.getInstance().getFilesWithKey(ConfigSchemaIndex.KEY, new HashSet<String>(Arrays.asList(contents)), new Processor<VirtualFile>() {
+                @Override
+                public boolean process(VirtualFile virtualFile) {
+
+                    PsiFile psiFile = PsiManager.getInstance(getProject()).findFile(virtualFile);
+                    if(psiFile == null) {
+                        return true;
+                    }
+
+                    YAMLDocument yamlDocument = PsiTreeUtil.getChildOfType(psiFile, YAMLDocument.class);
+                    if(yamlDocument == null) {
+                        return true;
+                    }
+
+                    for(YAMLKeyValue yamlKeyValue: PsiTreeUtil.getChildrenOfType(yamlDocument, YAMLKeyValue.class)) {
+                        String keyText = PsiElementUtils.trimQuote(yamlKeyValue.getKeyText());
+                        if(StringUtils.isNotBlank(keyText) && keyText.equals(contents)) {
+                            psiElements.add(yamlKeyValue);
+                        }
+                    }
+
+                    return true;
+                }
+            }, GlobalSearchScope.getScopeRestrictedByFileTypes(GlobalSearchScope.allScope(getProject()), YAMLFileType.YML));
+
+            return psiElements;
+        }
+    }
+
+}
---
+++ src/de/espend/idea/php/drupal/index/ConfigSchemaIndex.java
@@ -0,0 +1,139 @@
+package de.espend.idea.php.drupal.index;
+
+import com.intellij.ide.highlighter.XmlFileType;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.util.indexing.*;
+import com.intellij.util.io.DataExternalizer;
+import com.intellij.util.io.EnumeratorStringDescriptor;
+import com.intellij.util.io.KeyDescriptor;
+import fr.adrienbrault.idea.symfony2plugin.Symfony2ProjectComponent;
+import fr.adrienbrault.idea.symfony2plugin.stubs.indexes.ServicesDefinitionStubIndex;
+import fr.adrienbrault.idea.symfony2plugin.util.PsiElementUtils;
+import fr.adrienbrault.idea.symfony2plugin.util.yaml.YamlHelper;
+import gnu.trove.THashMap;
+import org.apache.commons.lang.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.yaml.YAMLFileType;
+import org.jetbrains.yaml.psi.YAMLDocument;
+import org.jetbrains.yaml.psi.YAMLFile;
+import org.jetbrains.yaml.psi.YAMLKeyValue;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+public class ConfigSchemaIndex extends FileBasedIndexExtension<String, String[]> {
+
+    public static final ID<String, String[]> KEY = ID.create("de.espend.idea.php.drupal.config_schema");
+    private final KeyDescriptor<String> myKeyDescriptor = new EnumeratorStringDescriptor();
+
+    @NotNull
+    @Override
+    public ID<String, String[]> getName() {
+        return KEY;
+    }
+
+    @NotNull
+    @Override
+    public DataIndexer<String, String[], FileContent> getIndexer() {
+        return new DataIndexer<String, String[], FileContent>() {
+            @NotNull
+            @Override
+            public Map<String, String[]> map(FileContent inputData) {
+                Map<String, String[]> map = new THashMap<String, String[]>();
+
+                PsiFile psiFile = inputData.getPsiFile();
+                if(!Symfony2ProjectComponent.isEnabledForIndex(psiFile.getProject())) {
+                    return map;
+                }
+
+                if(psiFile instanceof YAMLFile) {
+
+                    if(!isValidForIndex(inputData, psiFile)) {
+                        return map;
+                    }
+
+                    YAMLDocument yamlDocument = PsiTreeUtil.getChildOfType(psiFile, YAMLDocument.class);
+                    if(yamlDocument == null) {
+                        return map;
+                    }
+
+                    for(YAMLKeyValue yamlKeyValue: PsiTreeUtil.getChildrenOfType(yamlDocument, YAMLKeyValue.class)) {
+                        String key = PsiElementUtils.trimQuote(yamlKeyValue.getKeyText());
+
+                        if(StringUtils.isNotBlank(key) && !key.contains("*")) {
+                            Collection<String> mappings = new ArrayList<String>();
+                            YAMLKeyValue mapping = YamlHelper.getYamlKeyValue(yamlKeyValue, "mapping");
+                            if(mapping != null) {
+                                Set<String> keySet = YamlHelper.getKeySet(mapping);
+                                if(keySet != null) {
+                                    mappings.addAll(keySet);
+                                }
+                            }
+
+                            map.put(key, mappings.toArray(new String[mappings.size()]));
+                        }
+                    }
+
+                    return map;
+                }
+
+                return map;
+            }
+
+        };
+
+    }
+
+    @NotNull
+    @Override
+    public KeyDescriptor<String> getKeyDescriptor() {
+        return this.myKeyDescriptor;
+    }
+
+    @NotNull
+    @Override
+    public DataExternalizer<String[]> getValueExternalizer() {
+        return new ServicesDefinitionStubIndex.MySetDataExternalizer();
+    }
+
+    @NotNull
+    @Override
+    public FileBasedIndex.InputFilter getInputFilter() {
+        return new FileBasedIndex.InputFilter() {
+            @Override
+            public boolean acceptInput(@NotNull VirtualFile file) {
+                return file.getFileType() == YAMLFileType.YML || file.getFileType() == XmlFileType.INSTANCE;
+            }
+        };
+    }
+
+    @Override
+    public boolean dependsOnFileContent() {
+        return true;
+    }
+
+    @Override
+    public int getVersion() {
+        return 1;
+    }
+
+    public static boolean isValidForIndex(FileContent inputData, PsiFile psiFile) {
+
+        String fileName = psiFile.getName();
+        if(fileName.startsWith(".") || !fileName.endsWith(".schema.yml")) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+
+}
+
+
+
