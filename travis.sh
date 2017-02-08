a9868d37d9aaf26a06db8d5689cfaca87dc076bd
Haehnchen <daniel@espendiller.net>
2017-02-08 19:36:19 +0100

    IntelliJ build maintenance

--- travis.sh
+++ travis.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ideaVersion="2016.1"
+ideaVersion="2016.3.1"
 if [ "$PHPSTORM_ENV" == "2016.3.1" ]; then
-    ideaVersion="2016.3.1"
+    ideaVersion="2016.3.3"
 elif [ "$PHPSTORM_ENV" == "2016.3.2" ]; then
-    ideaVersion="2016.3.2"
+    ideaVersion="2016.3.3"
 elif [ "$PHPSTORM_ENV" == "eap" ]; then
     ideaVersion="163.5644.15"
 fi
